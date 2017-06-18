88a1ec54ae8ddb58905d51e4a63eb96093df29d4
pinkfloyd92 <ascacere92@gmail.com>
2017-06-18 17:56:52 -0500

    added all the processes to the list

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -4,27 +4,40 @@ ssize_t read;
 
 void fill_list_buf(head_b *hb){
 
-  const char *cmd = "ps up $(pgrep -f _hungry) 2>&-;";
+  const char *cmd = "ps -o pid,pcpu=CPU_USAGE,pmem,comm $(pgrep -f _hungry) 2>&-;";
   char *buf;//Output of the command
   FILE *ptr;
+  char * pch;
+  char* tmp_buf;
   size_t bufsize = BUFSIZE;
   struct buffer *item_buf; // struct that will be inside of our list.
-
+  tmp_buf = (char*)malloc(bufsize*sizeof(char));
 
   if (NULL == (ptr = popen(cmd, "r"))) {
     printf("An error ocurred");
     exit(EXIT_FAILURE);
   }
 
+  buf = (char *)malloc(bufsize * sizeof(char));
   while((read = getline(&buf, &bufsize, ptr))!= -1) {
-    buf = (char *)malloc(bufsize * sizeof(char));
     item_buf = (struct buffer*)malloc(sizeof(struct buffer));
     item_buf->line = buf;
     LIST_INSERT_HEAD(hb, item_buf, buffers);
+    buf = (char *)malloc(bufsize * sizeof(char));
   }
 
   LIST_FOREACH(item_buf, hb, buffers){
+
+    strcpy(tmp_buf, item_buf->line);
+    pch = strtok(tmp_buf," ");
+
     printf("Linea: %s\n", item_buf->line);
+    while (pch != NULL)
+      {
+	printf ("%s\n",pch);
+	pch = strtok (NULL, " ");
+      }
+
   }
 
   free(item_buf);
